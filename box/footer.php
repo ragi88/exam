8ac726a6892476fb4da84480fd4cabc1e244540c
ragi88 <raginasakha@gmail.com>
2021-09-09 12:34:41 +0545

    first commit

---
+++ box/footer.php
@@ -0,0 +1,10 @@
+<!--Footer Starts Here-->
+        <footer class="footer">
+            <div class="wrapper">
+                <p>
+                    Padmakanya Multiple Campus</a> &copy; <?php echo date('Y'); ?>. All Rights Reserved.
+                </p>
+            </div>
+        </footer>
+        <!--Footer Ends Here-->
+</html>
---
+++ box/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <!--Meta Tags Starts Here-->
+        <meta charset="UTF-8" />
+        <meta name="author" content="Padmakanya Mutiple Campus - Online Examination System" />
+        <meta name="description" content="Online Examination Portal by Ragina Saakha." />
+        <meta name="keywords" content="Online Examination Preaparation, Online Portal, Online Exam System, Ragina Saakha, Kathmandu, Nepal" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <!--Meta Tags Ends Here-->
+        <title>Online Examination System - Ragina Saakha</title>
+
+        <!--COUNT DOWN TIMER STARTS HERE-->
+        <script src="<?php echo SITEURL; ?>/assets/js/countdown/jquery.js"></script>
+        <script src="<?php echo SITEURL; ?>/assets/js/countdown/jquery.simple.timer.js"></script>
+        <script>
+          $(function(){
+            $('.timer').startTimer();
+          });
+        </script>
+        <!--COUNT DOWN TIMER ENDS HERE-->
+        
+        <!--ADDING CKEDITOR HERE-->
+        <script type="text/javascript" src="<?php echo SITEURL; ?>/assets/ckeditor/ckeditor.js"></script>
+        
+        <!--CSS File Starts Here-->
+        <link rel="stylesheet" type="text/css" href="<?php echo SITEURL; ?>/assets/css/style.css" />
+        <!--CSS File Ends Here-->
+    </head>
+    
+    <body>
+        <!--Header Starts Here-->
+        <header class="header">
+            <div class="wrapper clearfix ">
+                <!--<div class="logo">
+                    <img src="<?php echo SITEURL; ?>/images/logo.jpg" alt="Ragina Saakha" title="Online Examination System - Ragina Saakha" />
+                </div>-->
+
+                <div class="head-title">
+                    <h1>E-Campus</h1>
+                </div>
+            </div>
+        </header>
+        <!--Header Ends Here-->
